f47d880acb13a4ec5d425d6723d3942a97ef1857
Lucas Estrada <101399158+Kasssio@users.noreply.github.com>
2025-06-22 14:23:18 -0300

    orden de complejidad añadido (falta comp final)

--- billetera.cpp
+++ billetera.cpp
@@ -26,13 +26,13 @@ Billetera::Billetera(const id_billetera id, Blockchain* blockchain)
   {}
 
 id_billetera Billetera::id() const {
-  return _id;
+  return _id; // O(1)
 }
 
 void Billetera::notificar_transaccion(Transaccion t) {
-  _recientes.push_back(t);
-  if (t.origen == _id) {
-    _saldo-=t.monto;
+  _recientes.push_back(t); // O(1)
+  if (t.origen == _id) { // O(1)
+    _saldo-=t.monto; // O(1)
     int cant = _cantidad_transacciones[t.destino] + 1; // O(Log(C))
     _cantidad_transacciones[t.destino] = cant; // O(Log(C))
 
@@ -47,25 +47,22 @@ void Billetera::notificar_transaccion(Transaccion t) {
       }
     }
 
-    _cantidad_transacciones_invertido[cant-1] = cantidad_anterior_vector;
+    _cantidad_transacciones_invertido[cant-1] = cantidad_anterior_vector; // O(Log(C))
     
     _cantidad_transacciones_invertido[cant].push_back(t.destino); // O(Log(C))
-    _frecuentes = {};
+    _frecuentes = {}; // O(1)
 
     for(auto it = _cantidad_transacciones_invertido.rbegin(); it != _cantidad_transacciones_invertido.rend(); it ++){ // O(C)
       _frecuentes.insert(_frecuentes.end(),it->second.begin(), it->second.end()); // O(it->second) con it->second < C
     }
 
-  }
+  } 
   if (t.destino == _id) {
-    _saldo+=t.monto;
+    _saldo+=t.monto; // O(1)
   }
-
-
-  // Hay que arreglar esa parte que dura 150ms aprox
   // Todo el for es O(D*Log(D))
-  timestamp unDia = Calendario::dia(1);
-  timestamp finalDeHoy = Calendario::fin_del_dia(Calendario::tiempo_actual());
+  timestamp unDia = Calendario::dia(1); // O(1)
+  timestamp finalDeHoy = Calendario::fin_del_dia(Calendario::tiempo_actual()); // O(1)
 
 
   for (timestamp d = unDia; d < finalDeHoy ; d+=unDia){ // O(D)
@@ -77,10 +74,10 @@ void Billetera::notificar_transaccion(Transaccion t) {
   
   timestamp fin_del_dia = Calendario::fin_del_dia(t._timestamp); // O(1)
   _saldo_por_dia[fin_del_dia] = _saldo; // log(D)
-}
+} // Complejdad final: O()
 
 monto Billetera::saldo() const {
-  return _saldo;
+  return _saldo; // O(1)
 }
 
 monto Billetera::saldo_al_fin_del_dia(timestamp t) const {
@@ -92,62 +89,24 @@ monto Billetera::saldo_al_fin_del_dia(timestamp t) const {
 }
 
 vector<Transaccion> Billetera::ultimas_transacciones(int k) const {
-  auto it = _recientes.rbegin();
-  vector<Transaccion> ret;
-  while (it != _recientes.rend() && ret.size() < k){
-    ret.push_back(*it);
-    it++;
+  auto it = _recientes.rbegin(); // O(1)
+  vector<Transaccion> ret; // O(1)
+  while (it != _recientes.rend() && ret.size() < k){ // O(k)
+    ret.push_back(*it); // O(1)
+    it++; // O(1)
   }
-  return ret;
-}
+  return ret; // O(1)
+} // Complejidad final: O()
 
 vector<id_billetera> Billetera::detinatarios_mas_frecuentes(int k) const {
   
-  vector<id_billetera> ret;
-  int i = 0;
-  int s = _frecuentes.size();
-  while (i < s && i < k){
-    ret.push_back(_frecuentes[i]);
-    i++;
+  vector<id_billetera> ret; // O(1)
+  int i = 0; // O(1)
+  int s = _frecuentes.size(); // O(1)
+  while (i < s && i < k){ // O(k)
+    ret.push_back(_frecuentes[i]); // O(1)
+    i++; // O(1)
   }
-  return ret;
+  return ret; // O(1)
 
-
-  // const list<Transaccion> transacciones = _blockchain->transacciones();
-
-  // // cuento la cantidad de transacciones salientes por cada billetera de destino
-  // map<id_billetera, int> transacciones_por_billetera;
-  // auto it = transacciones.begin();
-  // while (it != transacciones.end()) {
-  //   if (it->origen == _id) {
-  //     // notar que el map devuelve 0 por default!
-  //     transacciones_por_billetera[it->destino]++;
-  //   }
-  //   ++it;
-  // }
-
-  // // invierto el map de forma que puedo accedes a las billeteras según su
-  // // cantidad de transacciones.
-  // map<int, vector<id_billetera>> billeteras_por_cantidad_de_transacciones;
-  // auto it2 = transacciones_por_billetera.begin();
-  // while (it2 != transacciones_por_billetera.end()) {
-  //   billeteras_por_cantidad_de_transacciones[it2->second].push_back(it2->first);
-  //   ++it2;
-  // }
-
-  // // recorro el map usando un iterador en orden inverso, que me lleva por todos
-  // // los pares de entradas desde la mayor clave hasta la menor.
-  // vector<id_billetera> ret = {};
-  // auto it3 = billeteras_por_cantidad_de_transacciones.rbegin();
-  // while (it3 != billeteras_por_cantidad_de_transacciones.rend() && ret.size() < k) {
-  //   vector<id_billetera> siguiente_grupo = it3->second;
-  //   int i = 0;
-  //   while (i < siguiente_grupo.size() && ret.size() < k) {
-  //     ret.push_back(siguiente_grupo[i]);
-  //     i++;
-  //   }
-  //   ++it3;
-  // }
-
-  // return ret;
 }
